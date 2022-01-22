531fa1023f7fc47d3a6eae78d83b8b1cae0be7d9
andrewhumble <andrewhumble@me.com>
2022-01-22 17:59:54 -0500

    Testing mobile timer display

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CountDownTimer from "./CountDownTimer";
 import { Typography, Grid } from "@material-ui/core";
 import Header from "./Header";
@@ -15,71 +15,165 @@ const Work = ({
 }) => {
   const hoursMinSecs = { hours: 0, minutes: values.workTime, seconds: 5 };
 
-  return (
-    <div>
-      <Header logout={logout} values={values} />
-      <div
-        style={{
-          position: "absolute",
-          left: "50%",
-          top: "40%",
-          transform: "translate(-50%, -50%)",
-          fontFamily: "Helvetica",
-          fontWeight: "bold",
-          fontSize: "200px",
-          color: "#1DB954",
-        }}
-      >
-        <Grid direction="column" alignItems="center" justifyContent="center">
-          <Grid>
-            <Typography
-              component="h1"
-              variant="h4"
-              align="center"
-              style={{
-                position: "absolute",
-                left: "40%",
-                top: "25%",
-                fontFamily: "Helvetica",
-                fontWeight: "bold",
-                fontSize: "30px",
-                color: "#191414",
-              }}
-            >
-              get to work
-            </Typography>
+  const [state, setState] = useState({
+    mobileView: false,
+  });
+
+  const { mobileView } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 1000
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
+  const displayDesktop = () => {
+    return (
+      <div>
+        <Header logout={logout} values={values} />
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "40%",
+            transform: "translate(-50%, -50%)",
+            fontFamily: "Helvetica",
+            fontWeight: "bold",
+            fontSize: "200px",
+            color: "#1DB954",
+          }}
+        >
+          <Grid direction="column" alignItems="center" justifyContent="center">
+            <Grid>
+              <Typography
+                component="h1"
+                variant="h4"
+                align="center"
+                style={{
+                  position: "absolute",
+                  left: "40%",
+                  top: "25%",
+                  fontFamily: "Helvetica",
+                  fontWeight: "bold",
+                  fontSize: "30px",
+                  color: "#191414",
+                }}
+              >
+                get to work
+              </Typography>
+            </Grid>
+            <Grid>
+              <CountDownTimer
+                hoursMinSecs={hoursMinSecs}
+                setStep={setStep}
+                values={values}
+                pause={pause}
+                play={play}
+                incrementSessionCount={incrementSessionCount}
+              />
+            </Grid>
+            <Grid>
+              <Typography
+                component="p"
+                align="center"
+                style={{
+                  position: "absolute",
+                  left: "35%",
+                  top: "70%",
+                  fontFamily: "Helvetica",
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                  color: "darkgrey",
+                }}
+              >
+                number of Pomodoros completed: {values.sessions - 1}
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid>
-            <CountDownTimer
-              hoursMinSecs={hoursMinSecs}
-              setStep={setStep}
-              values={values}
-              pause={pause}
-              play={play}
-              incrementSessionCount={incrementSessionCount}
-            />
-          </Grid>
-          <Grid>
-            <Typography
-              component="p"
-              align="center"
-              style={{
-                position: "absolute",
-                left: "35%",
-                top: "70%",
-                fontFamily: "Helvetica",
-                fontWeight: "bold",
-                fontSize: "15px",
-                color: "darkgrey",
-              }}
-            >
-              number of Pomodoros completed: {values.sessions - 1}
-            </Typography>
-          </Grid>
-        </Grid>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  const displayMobile = () => {
+    return (
+      <div>
+        <Header logout={logout} values={values} />
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "40%",
+            transform: "translate(-50%, -50%)",
+            fontFamily: "Helvetica",
+            fontWeight: "bold",
+            fontSize: "40px",
+            color: "#1DB954",
+          }}
+        >
+          <Grid direction="column" alignItems="center" justifyContent="center">
+            <Grid>
+              <Typography
+                component="h1"
+                variant="h4"
+                align="center"
+                style={{
+                  position: "absolute",
+                  left: "40%",
+                  top: "25%",
+                  fontFamily: "Helvetica",
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                  color: "#191414",
+                }}
+              >
+                get to work
+              </Typography>
+            </Grid>
+            <Grid>
+              <CountDownTimer
+                hoursMinSecs={hoursMinSecs}
+                setStep={setStep}
+                values={values}
+                pause={pause}
+                play={play}
+                incrementSessionCount={incrementSessionCount}
+              />
+            </Grid>
+            <Grid>
+              <Typography
+                component="p"
+                align="center"
+                style={{
+                  position: "absolute",
+                  left: "35%",
+                  top: "70%",
+                  fontFamily: "Helvetica",
+                  fontWeight: "bold",
+                  fontSize: "10px",
+                  color: "darkgrey",
+                }}
+              >
+                number of Pomodoros completed: {values.sessions - 1}
+              </Typography>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    );
+  };
+
+  return <div>{mobileView ? displayMobile() : displayDesktop()}</div>;
 };
 
 export default Work;
