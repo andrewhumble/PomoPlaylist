8e20f92b0ae6bc659268ff4bdb14df666e6afef0
andrewhumble <43395285+andrewhumble@users.noreply.github.com>
2024-08-26 19:42:19 -0400

    Add back

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PomoPlay from "./components/PomoPlay";
 import { Helmet } from "react-helmet";
 import { ReactNotifications } from "react-notifications-component";
 import { PomoProvider } from "./components/PomoContext";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -13,7 +14,11 @@ function App() {
         <Helmet>
           <style>{"body { background-color: #121212; }"}</style>
         </Helmet>
-        <PomoPlay />
+        {/* Set up routing */}
+        <Routes>
+          <Route path="/" element={<PomoPlay />} />
+          {/* Add other routes here if needed */}
+        </Routes>
       </div>
     </PomoProvider>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
+  <Router> {/* Wrap your App in Router */}
+    <App />
+  </Router>
 );
 
 reportWebVitals();
